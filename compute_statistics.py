b0285433324a96d8ee0495d84541e89452338b76
daoterog <doterog320@gmail.com>
2023-03-15 10:27:12 -0500

    Add median

--- compute_statistics.py
+++ compute_statistics.py
@@ -1,2 +1,9 @@
 def compute_mean(array: list) -> float:
     return sum(array) / len(array)
+
+def compute_median(array: list) -> float:
+    array = sorted(array)
+    if len(array) % 2 == 0:
+        return (array[len(array) // 2] + array[len(array) // 2 - 1]) / 2
+    else:
+        return array[len(array) // 2]
